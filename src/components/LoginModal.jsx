f8eef44568ff846d33d4ecd921eba20575c134b5
John Doe <johndoe@example.com>
2023-01-27 12:47:01 +0100

    register finish

--- src/components/LoginModal.jsx
+++ src/components/LoginModal.jsx
@@ -1,10 +1,49 @@
-import React, { useState } from 'react';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import React,{useState} from "react";
+import { checkUsername,login } from "./getData";
+import {useMutation} from 'react-query';
+import {useNavigate} from 'react-router-dom'
 
 export const LoginModal=({loginmodal,setLoginModal}) => {
-  
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const [isValidU, setIsValidU] = useState(null)
+  const [isValidP, setIsValidP] = useState(null)
+  const navigate=useNavigate()
 
   const toggle = () => setLoginModal(!loginmodal);
+
+  const mutationCheckUsername=useMutation(checkUsername,{
+    onSuccess:(data)=>{
+      console.log(data.data.rowCount,data.data.username)
+      if(data.data.rowCount==0)
+        setIsValidU(false)
+      else
+        setIsValidU(true)
+    }
+  })
+
+  const handleCheckUsername = () =>{
+    if(username)
+      mutationCheckUsername.mutate({username:username})
+    else
+      setIsValidU(false)
+  }
+
+  const mutationLogin=useMutation(login,{
+    onSuccess:(data) =>{
+      console.log(data.data)
+      if(data.data?.error)
+        setIsValidP(false)
+      else{
+        setIsValidP(true)
+        const {username,email,id,avatar,avatar_id} = data.data
+        setLoginModal({username:username,email:email,id:id,avatar:avatar,avatar_id:avatar_id})
+        navigate('/')
+      }
+       
+    }
+  })
 
   return (
     <div>
@@ -13,18 +52,25 @@ export const LoginModal=({loginmodal,setLoginModal}) => {
         <ModalBody className='reg2'>A bejelentkezési adatokat , kérem helyesen adja meg ! <br /> <br /> <br />
 
             <label className='logpanel2' for="username"><b>Felhasználónév:</b></label><br />
-            <input className='logpanel' type="text" placeholder="Írd be a felhasználó neved" name="username" required/> <br />
+            <input className={isValidU==null ? "" : (isValidU ? "is-valid" : "is-invalid")}
+            autoFocus
+            value={username} onChange={(e)=>setUsername(e.target.value)}
+            onBlur={handleCheckUsername}
+            onKeyPress={(e)=>e.key=='Enter' ? document.getElementById("password").focus() : ''}/>
+            <br />
             <label className='logpanel2' for="password"><b>Jelszó:</b></label><br />
-            <input  className='logpanel' type="text" placeholder="Írd be a jelszavad" name="password" required/> <br />
+            <input type="password" className= {isValidP==null ? "" : (isValidP ? "is-valid" : "is-invalid")}
+            id="password"
+            value={password} onChange={(e)=>setPassword(e.target.value)}
+            onKeyPress={(e)=>e.key=='Enter' ? document.getElementById("login").focus() : ''}/>
+            <br />
         </ModalBody>
-        <ModalFooter>
-          <Button color="secondary" onClick={toggle}>
-            Bejelentkezés
-          </Button>{' '}
-          <Button color="danger" onClick={toggle}>
-            Mégse
-          </Button>
-        </ModalFooter>
+        <div>
+        <Button disabled={!isValidU || !password} color="dark"
+        id="login"
+        onClick={()=>mutationLogin.mutate({username:username, password:password})}
+        >Login</Button>
+      </div>
       </Modal>
     </div>
   );
--- src/components/RegisterModal.jsx
+++ src/components/RegisterModal.jsx
@@ -1,12 +1,88 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
+import { checkUsername } from "./getData";
+import {useMutation} from 'react-query';
+import { checkEmail } from "./getData";
+import { validate } from 'react-email-validator';
+import { register } from "./getData";
+import {
+  Input,  
+} from "reactstrap";
+
+
 
 export const MyModal=({modal,setModal}) => {
   const navigate = useNavigate()
-  
+  const [isValidU,setIsValidU] = useState(null)
+  const [username,setUsername] = useState('')
+  const [email,setEmail] = useState('')
+  const [isValidE,setIsValidE] = useState(null)
+  const [password,setPassword] = useState('')
+  const [isValidP,setIsValidP] = useState(null)
+  const [success,setSuccess] = useState(null)
+  const [msg,setMsg] = useState('')
+
+
+
+
 
   const toggle = () => setModal(!modal);
+
+  const mutationCheckUsername=useMutation(checkUsername,{
+    onSuccess:(data)=>{
+      console.log(data.data.rowCount,data.data.username)
+      if(data.data.rowCount==0)
+        setIsValidU(true)
+      else
+        setIsValidU(false)
+    }
+  })
+  const handleCheckUsername = () =>{
+    if(username)
+      mutationCheckUsername.mutate({username:username})
+    else
+      setIsValidU(false)
+  }
+
+  const handleCheckEmail = () =>{
+    if(validate(email))
+      mutationCheckEmail.mutate({email:email})
+    else
+      setIsValidE(false)
+  }
+
+  const mutationCheckEmail=useMutation(checkEmail,{
+    onSuccess:(data)=>{
+      console.log(data.data.rowCount,data.data.email)
+      if(data.data.rowCount==0)
+        setIsValidE(true)
+      else
+        setIsValidE(false)
+    }
+  })
+
+  const handleCheckPassword = () =>{
+    password.length<6 ? setIsValidP(false) : setIsValidP(true)
+  }
+
+  const mutationCheckRegister=useMutation(register,{
+    onSuccess:(data)=>{
+      if(data.data?.id){
+        setSuccess(true)
+        setUsername('')
+        setPassword('')
+        setEmail('')
+        setIsValidU(null)
+        setIsValidP(null)
+        setIsValidE(null)
+      }
+      else{
+        setSuccess(false)
+      }
+      setMsg(data.data.msg)
+    }
+  })
 
   return (
     <div className='mymodal bg-light'>
@@ -15,22 +91,46 @@ export const MyModal=({modal,setModal}) => {
         <ModalBody className='reg2'>A mezők kitőltése kötelező! <br /> <br /> 
 
         <label className='regpanel2' for="username"><b>Felhasználónév:</b></label><br />
-            <input className='regpanel' type="text" placeholder="Írd be a felhasználó neved" name="username" required/> <br />
+            <input  className= {isValidU==null ? "" :(isValidU ? "is-valid" : "is-invalid")}
+          onKeyPress={(e)=>e.key=='Enter' ? document.getElementById("email").focus() : ''}
+          onBlur={handleCheckUsername}
+          value={username} 
+          autoFocus 
+          onChange={(e)=>setUsername(e.target.value)} />
+          <br />
             <label className='regpanel2' for="password"><b>Jelszó: <br /> </b></label><br />
             <p>* Olyan jelszót válassz amelyet még nem használtál más felületen, a visszaélések elkerülésének érdekében (az ilyen visszaélésekkel kapcsolatosan nem áll módunkban felelősséget vállalni)!</p>
-            <input className='regpanel' type="password" placeholder="Írd be a jelszavad" name="password" required/> <br />
-            <label className='regpanel2' for="Email:"><b>E-mail:</b></label><br />
-            <input className='regpanel' type="text" placeholder="Írd be az Email-címet" name="email" required/> <br />
+            <input type="password" 
+        id="password" 
+        className= {isValidP==null ? "" :(isValidP ? "is-valid" : "is-invalid")}
+        onBlur={handleCheckPassword}
+        value={password}
+        onKeyPress={(e)=>e.key=='Enter' ? document.getElementById("email").focus() : ''} 
+        onChange={(e)=>setPassword(e.target.value)} /> <br />
+            <label className='regpanel2' for="email"><b>E-mail:</b></label><br />
+            <input type="email" 
+        id="email" 
+        className={isValidE==null ? "" :(isValidE ? "is-valid" : "is-invalid")} 
+        onBlur={handleCheckEmail} 
+        onKeyPress={(e)=>e.key=='Enter' ? document.getElementById("password").focus() : ''} 
+        value={email} 
+        onChange={(e)=>setEmail(e.target.value)} /> <br />
 
         </ModalBody>
-        <ModalFooter>
-          <Button color="secondary" onClick={toggle}>
-            Regisztrálok
-          </Button>{' '}
-          <Button color="danger" onClick={toggle}>
-            Bezárás
-          </Button>
-        </ModalFooter>
+
+        <div className="text-center">
+      <Input 
+      id="done"
+      type="button" 
+      disabled={!isValidU || !isValidP || !isValidE} 
+      value="Register" 
+      className="btn btn-dark"
+      onClick={()=>mutationCheckRegister.mutate({username:username,email:email,password:password})}
+      />
+      </div>
+      <h4 className="msg text-center m-3 text-success">{msg}</h4>
+      
+
       </Modal>
     </div>
   );
