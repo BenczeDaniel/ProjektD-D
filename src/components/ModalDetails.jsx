ce3f290f8f5959bf2ef178d911bab337950a06ad
Bencze Daniel <benczedaniel13@gmail.com>
2023-03-04 18:34:50 +0100

    modal on video

--- src/components/ModalDetails.jsx
+++ src/components/ModalDetails.jsx
@@ -1,24 +1,35 @@
 import React, { useState } from 'react';
 import { Button, Modal, ModalHeader, ModalBody, ModalFooter } from 'reactstrap';
 
+
 export const ModalDetails=({modal,setModal,description})  =>{
  
 
+
+
   const toggle = () => setModal(!modal);
 
-  return (
-    <div>
-      <Modal isOpen={modal} toggle={toggle}>
-        <ModalHeader toggle={toggle}>Modal title</ModalHeader>
-        <ModalBody>
-         {description}
-        </ModalBody>
-        <ModalFooter>
 
-          <Button color="secondary" onClick={toggle}>
-            Cancel
-          </Button>
-        </ModalFooter>
+ 
+  return (
+
+    
+  
+    <div >
+      <Modal  isOpen={modal} toggle={toggle}>
+        <ModalHeader className='text-white ' toggle={toggle}>Videó az edzésről!</ModalHeader>
+        <ModalBody className='videotext '  > 
+        
+        <iframe className='videoborder' id="video" width="500" height="350"
+                                    src={description}
+                                    
+ 
+                                    frameborder="0" allowfullscreen>
+                            </iframe>
+
+   
+        </ModalBody > 
+
       </Modal>
     </div>
   );
